4b98c5067e838221934704fd290fbe4c9a7382f8
Sohambasu07 <soham.basu07@gmail.com>
2024-12-04 00:03:11 +0100

    Changed default root

--- src/momfpriors/constants.py
+++ src/momfpriors/constants.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from pathlib import Path
 
-DEFAULT_ROOT_DIR = Path.cwd().absolute().parent.parent / "results"
+DEFAULT_ROOT_DIR = Path.cwd().absolute().parent / "momf_priors_results"
