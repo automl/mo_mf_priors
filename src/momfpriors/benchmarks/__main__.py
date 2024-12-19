f76ebdaeba9093667d5333ee4180ac152e81f2b0
Sohambasu07 <soham.basu07@gmail.com>
2024-12-19 01:39:05 +0100

    benchmark __main__ to print available list

---
+++ src/momfpriors/benchmarks/__main__.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import argparse
+import logging
+
+from momfpriors.benchmarks import BENCHMARKS
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def main(
+    list: str
+):
+    match list:
+        case "all":
+            logger.info(BENCHMARKS.keys())
+        case _:
+            logger.error(f"Invalid list type: {list}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--list",
+        required=True,
+        choices=["all"],
+        help="List all Benchmarks or filter by type"
+        "\nall: list all Benchmarks"
+    )
+    args = parser.parse_args()
+    main(args.list)
