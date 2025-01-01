5f106d9f86031ad205d9b5a684eb2d9911b50847
Sohambasu07 <soham.basu07@gmail.com>
2025-01-01 23:29:21 +0100

    Modified constants

--- src/momfpriors/constants.py
+++ src/momfpriors/constants.py
@@ -2,4 +2,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-DEFAULT_ROOT_DIR = Path.cwd().absolute().parent / "momf_priors_results"
+DEFAULT_ROOT_DIR = Path.cwd().absolute()
+DEFAULT_PRIORS_DIR = DEFAULT_ROOT_DIR / "src" / "priors"
+DEFAULT_RESULTS_DIR = DEFAULT_ROOT_DIR.parent / "nomf_priors_results"
