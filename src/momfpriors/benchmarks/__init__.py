ffa7f400049593d97140f3be6a5407285d5a612f
Sohambasu07 <soham.basu07@gmail.com>
2025-01-17 15:18:06 +0100

    Removing hpobench and bbob for initial runs

--- src/momfpriors/benchmarks/__init__.py
+++ src/momfpriors/benchmarks/__init__.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
 
-from momfpriors.benchmarks.bbob_mo import bbob_mo_benchmarks
+# from momfpriors.benchmarks.bbob_mo import bbob_mo_benchmarks
 from momfpriors.benchmarks.botorch_momf import MOMFBC_Bench, MOMFPark_Bench
-from momfpriors.benchmarks.hpobench import hpobench_benchmarks
+# from momfpriors.benchmarks.hpobench import hpobench_benchmarks
 
 BENCHMARKS = {
     MOMFBC_Bench.desc.name: MOMFBC_Bench,
     MOMFPark_Bench.desc.name: MOMFPark_Bench,
 }
 
-for desc in bbob_mo_benchmarks():
-    BENCHMARKS[desc.name] = desc
-for desc in hpobench_benchmarks():
-    BENCHMARKS[desc.name] = desc
+# for desc in bbob_mo_benchmarks():
+#     BENCHMARKS[desc.name] = desc
+# for desc in hpobench_benchmarks():
+#     BENCHMARKS[desc.name] = desc
 
 __all__ = ["BENCHMARKS"]
