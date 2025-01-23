26cbfd3f018f632bb23cfd277dbfb0cbe1b9c399
Sohambasu07 <soham.basu07@gmail.com>
2025-01-23 14:54:42 +0100

    Plots for multiple pareto fronts

--- src/momfpriors/plotting.py
+++ src/momfpriors/plotting.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import argparse
 import logging
+from collections.abc import Mapping
 from pathlib import Path
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,7 +13,7 @@ import pandas as pd
 from momfpriors.constants import DEFAULT_RESULTS_DIR
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 
 def pareto_front(
@@ -28,27 +30,45 @@ def pareto_front(
             is_pareto[i] = True
     return is_pareto
 
+
 def plot_pareto(
-    results: pd.Series,
-    run_dir: Path,
-    plot_title: str,
+    data: Mapping[str, Mapping[str, Any]],
+    exp_dir: Path,
+    benchmark: str,
 ) -> None:
     """Function to plot the pareto front from a pandas Series
     of Results, i.e., Mapping[str, float] objects.
     """
-    keys = list(results[0].keys())
-    assert len(keys) == 2, "Can only plot pareto front for 2D cost space."  # noqa: PLR2004
-    pareto = pareto_front(results)
-    pareto = np.array([list(res.values()) for res in results])[pareto]
-    logger.info(f"Plotting pareto front for {plot_title}")
-    plt.scatter(pareto[:, 0], pareto[:, 1])
+    logger.info(f"\nPlots for runs on benchmark: {benchmark}")
+    plt.figure(figsize=(10, 10))
+    for instance, instance_data in data.items():
+        logger.info(f"Plotting pareto front for {instance}")
+        results = instance_data["results"]
+        plot_title = instance_data["plot_title"]
+        keys = list(results[0].keys())
+        assert len(keys) == 2, "Can only plot pareto front for 2D cost space."  # noqa: PLR2004
+        pareto = pareto_front(results)
+        pareto = np.array([list(res.values()) for res in results])[pareto]
+        pareto = pareto[pareto[:, 0].argsort()]
+        logger.info(f"Plotting pareto front for {plot_title}")
+        plt.plot(
+            pareto[:, 0],
+            pareto[:, 1],
+            marker="o",
+            markersize=7,
+            linewidth=1,
+        )
+    plt.grid()
+    plt.legend(list(data.keys()))
     plt.xlabel(keys[0])
     plt.ylabel(keys[1])
+    if len(data) > 1:
+        plot_title = f"Pareto front for Optimizers on {benchmark} benchmark"
     plt.title(plot_title)
-    save_dir = run_dir / "plots"
+    save_dir = exp_dir / "plots"
     save_dir.mkdir(parents=True, exist_ok=True)
-    plt.savefig(save_dir / "pareto_front.png")
-    plt.show()
+    plt.savefig(save_dir / f"{plot_title.replace(' ', '_')}.png")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -59,20 +79,36 @@ if __name__ == "__main__":
         help="Main experiment directory containing the runs to plot."
     )
     args = parser.parse_args()
-    exp_dir = DEFAULT_RESULTS_DIR / args.exp_dir
+    exp_dir: Path = DEFAULT_RESULTS_DIR / args.exp_dir
 
-    for run_dir in exp_dir.iterdir():
-        run = exp_dir / run_dir
-        for file in run.iterdir():
-            if file.suffix == ".parquet":
-                _df = pd.read_parquet(file)
-                results = _df["results"]
-                benchmark = _df["benchmark"][0]
-                optimizer = _df["optimizer"][0]
-                opt_hps = _df["optimizer_hps"][0]
-                plt_title = f"{optimizer}_{opt_hps} on {benchmark}"
-                plot_pareto(
-                    results=results,
-                    run_dir=run_dir,
-                    plot_title=plt_title,
-                )
+    agg_dict: Mapping[str, Mapping[str, Any]] = {}
+
+
+    benchmarks_in_dir = [
+        (f.name.split("benchmark=")[-1].split(".")[0])
+        for f in exp_dir.iterdir() if f.is_dir() and "benchmark=" in f.name]
+    benchmarks_in_dir = list(set(benchmarks_in_dir))
+    logger.info(f"Found benchmarks: {benchmarks_in_dir}")
+
+
+    for benchmark in benchmarks_in_dir:
+        for file in exp_dir.rglob("*.parquet"):
+            if benchmark not in file.name:
+                continue 
+            _df = pd.read_parquet(file)
+            instance = _df["optimizer"][0] + "_" + _df["optimizer_hps"][0]
+            agg_dict[instance] = {
+                "results": _df["results"],
+                "optimizer": _df["optimizer"][0],
+                "opt_hps": _df["optimizer_hps"][0],
+                "plot_title": (
+                    f"{_df['optimizer'][0]}_"
+                    f"{_df['optimizer_hps'][0]}"
+                    f" on {benchmark}"
+                ),
+            }
+        plot_pareto(
+            data=agg_dict,
+            exp_dir=exp_dir,
+            benchmark=benchmark,
+        )
