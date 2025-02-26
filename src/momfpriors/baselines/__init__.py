ded539b439fcc3a773a4d2d910d2737dca21bf91
Sohambasu07 <soham.basu07@gmail.com>
2025-02-26 10:22:34 +0100

    Added Neps Ask and Tell RW

--- src/momfpriors/baselines/__init__.py
+++ src/momfpriors/baselines/__init__.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from momfpriors.baselines.neps_optimizers import NepsOptimizer
+from momfpriors.baselines.neps_optimizers import NepsRW
 from momfpriors.baselines.random_search import RandomSearch, RandomSearchWithPriors
 from momfpriors.baselines.smac_parego import SMAC_ParEGO
 
 OPTIMIZERS = {
     RandomSearch.name: RandomSearch,
     RandomSearchWithPriors.name: RandomSearchWithPriors,
-    NepsOptimizer.name: NepsOptimizer,
+    NepsRW.name: NepsRW,
     SMAC_ParEGO.name: SMAC_ParEGO,
 }
 
--- src/momfpriors/baselines/neps_optimizers.py
+++ src/momfpriors/baselines/neps_optimizers.py
@@ -1,46 +1,25 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Callable, Mapping
-from functools import partial
+from collections.abc import Mapping
 from pathlib import Path
 from typing import Any
 
-import neps
 import numpy as np
-from hpoglue import Problem
+from hpoglue import Config, Problem, Query, Result
+from hpoglue.env import Env
+from neps import AskAndTell, algorithms
+from neps.space.parsing import convert_configspace
 
 from momfpriors.constants import DEFAULT_RESULTS_DIR
-from momfpriors.optimizer import Abstract_NonAskTellOptimizer
-from momfpriors.utils import objective_fn_wrapper
+from momfpriors.optimizer import Abstract_AskTellOptimizer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def neps_so_pipeline():
-    pass
-
-
-def neps_mo_scalarized(
-    objective_fn: Callable,
-    objectives: list[str],
-    scalarization_weights: Mapping[str, float],
-    **config: Mapping[str, Any]
-) -> Mapping[str, Any]:
-    results = objective_fn_wrapper(objective_fn, **config)
-    scalarized_objective = sum(
-        scalarization_weights[obj] * results[obj] for obj in objectives
-    )
-    logger.info([results[obj] for obj in objectives])
-    logger.info(f"{scalarized_objective=}")
-    return {
-        "loss": scalarized_objective,
-        "cost": None,
-        "info_dict": results
-    }
-
-class NepsOptimizer(Abstract_NonAskTellOptimizer):
+class NepsOptimizer(Abstract_AskTellOptimizer):
+    """Base class for Neps Optimizers."""
     name = "NepsOptimizer"
 
     support = Problem.Support(
@@ -48,6 +27,12 @@ class NepsOptimizer(Abstract_NonAskTellOptimizer):
         objectives=("many"),
         cost_awareness=(None,),
         tabular=False,
+    )
+
+    env = Env(
+        name="NEPS-0.12.3",
+        python_version="3.10",
+        requirements=("neural-pipeline-search==0.12.3",)
     )
 
     mem_req_mb = 1024
@@ -60,45 +45,90 @@ class NepsOptimizer(Abstract_NonAskTellOptimizer):
         **kwargs: Any,
     ) -> None:
         self.problem = problem
-        self.space = problem.config_space
+        self.space = convert_configspace(self.problem.config_space)
         if not isinstance(working_directory, Path):
             working_directory = Path(working_directory)
         self.searcher = self.problem.optimizer_hyperparameters.get(
-            "searcher", "random_search"
+            "searcher", "bayesian_optimization"
         )
-        self.name = f"NepsOptimizer_{self.searcher}"
+        kwargs.pop("searcher", None)
         self.seed = seed
         self.working_dir = working_directory / "neps_dir" / self.name
 
-
-    def optimize(
-        self,
-        scalarization_weights: Mapping[str, float] | None = None,
-    ) -> None:
-        objectives = self.problem.get_objectives()
-        objectives = objectives if isinstance(objectives, list) else [objectives]
-        benchmark = self.problem.benchmark.load(self.problem.benchmark)
-        if len(objectives) > 1:
-            if not scalarization_weights:
-                scalarization_weights = {
-                    f"{obj}": 1.0/len(objectives) for obj in objectives
-                }
-            run_pipeline = partial(
-                neps_mo_scalarized,
-                objective_fn=benchmark.query,
-                objectives=objectives,
-                scalarization_weights=scalarization_weights)
-        else:
-            run_pipeline = neps_so_pipeline
-
         np.random.seed(self.seed)  # noqa: NPY002
-        neps.run(
-            run_pipeline=run_pipeline,
-            pipeline_space=self.space,
-            root_directory=self.working_dir,
-            max_evaluations_total=self.problem.budget.total,
-            post_run_summary=True,
-            overwrite_working_directory=True,
-            searcher=self.searcher,
+        opt = algorithms.PredefinedOptimizers[self.searcher](
+            space = self.space,
+            **kwargs
+        )
+        self.optimizer = AskAndTell(opt)
+        self.trial_counter = 0
+
+
+    def ask(self) -> Query:
+        trial = self.optimizer.ask() # TODO: Figure out fidelity
+        return Query(
+            config = Config(config_id=self.trial_counter, values=trial.config),
+            fidelity=None,
+            optimizer_info=trial
         )
 
+    def tell(self, result: Result) -> None:
+        costs = list(result.values.values())
+        self.optimizer.tell(
+            trial=result.query.optimizer_info,
+            result=costs
+        )
+
+
+class NepsRW(NepsOptimizer):
+    """Random Weighted Scalarization of objectives using Bayesian Optimization in Neps."""
+
+    name = "NepsRW"
+
+    support = Problem.Support(
+        fidelities=(None,),
+        objectives=("many"),
+        cost_awareness=(None,),
+        tabular=False,
+    )
+
+    env = Env(
+        name="Neps-0.12.2",
+        python_version="3.10",
+        requirements=("neural-pipeline-search==0.12.2",)
+    )
+
+    mem_req_mb = 1024
+
+    def __init__(
+        self,
+        problem: Problem,
+        seed: int = 0,
+        working_directory: str | Path = DEFAULT_RESULTS_DIR,
+        scalarization_weights: Mapping[str, float] | None = None,
+        searcher: str = "bayesian_optimization",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            problem=problem,
+            seed=seed,
+            working_directory=working_directory,
+            searcher=searcher,
+        )
+        self.objectives = self.problem.get_objectives()
+        self.scalarization_weights = scalarization_weights
+        if not self.scalarization_weights:
+            self.scalarization_weights = {
+                obj: 1.0/len(self.objectives) for obj in self.objectives
+            }
+
+
+    def tell(self, result: Result) -> None:
+        costs = {obj: result.values[obj] for obj in self.objectives}
+        scalarized_objective = sum(
+            self.scalarization_weights[obj] * costs[obj] for obj in self.objectives
+        )
+        self.optimizer.tell(
+            trial=result.query.optimizer_info,
+            result=scalarized_objective
+        )
