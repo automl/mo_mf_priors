39ea72adf8acf45491487c44782e696bba69f104
Sohambasu07 <soham.basu07@gmail.com>
2025-03-30 14:28:01 +0200

    all_yahpo_lcbench task ids prior generation

--- scripts/meta/yahpo_lcbench_priors.sh
+++ scripts/meta/yahpo_lcbench_priors.sh
@@ -29,7 +29,7 @@ source ~/repos/momfp_env/bin/activate
 # Running the job
 start=`date +%s`
 
-python3 -m momfpriors.generate_priors -y configs/prior_gen/yahpo-lcbench.yaml -n 100000
+python3 -m momfpriors.generate_priors -y configs/prior_gen/all_yahpo_lcbench_ids.yaml -n 100000
 
 end=`date +%s`
 runtime=$((end-start))
