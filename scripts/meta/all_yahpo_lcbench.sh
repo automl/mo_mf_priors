22e14e5556a67ef8e5067cab76614fca59589a3d
Sohambasu07 <soham.basu07@gmail.com>
2025-03-30 14:00:24 +0200

    exp config and slurm batch script for all yahpo lcbench task ids

---
+++ scripts/meta/all_yahpo_lcbench.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the partition on which the job shall run.
+#SBATCH --partition bosch_cpu-cascadelake    # short: -p bosch_cpu-cascadelake
+
+# Define a name for your job
+#SBATCH --job-name all_yahpo_lcbench            #  short: -J all_yahpo_lcbench
+
+# Define the files to write the outputs of the job to.
+# Please note the SLURM will not create this directory for you, and if it is missing, no logs will be saved.
+# You must create the directory yourself. In this case, that means you have to create the "logs" directory yourself.
+
+#SBATCH --output logs/%x-%A_meta_yahpo.out   # STDOUT  %x and %A will be replaced by the job name and job id, respectively. short: -o logs/%x-%A_meta_yahpo.out
+#SBATCH --error logs/%x-%A_meta_yahpo.err    # STDERR  short: -e logs/%x-%A_meta_yahpo.out
+
+# Since using CPU Partion, define the number of cpus required per node
+#SBATCH --cpus-per-task 30
+
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+
+# A few SLURM variables
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+# Activate your environment
+# You can also comment out this line, and activate your environment in the login node before submitting the job
+source ~/repos/momfp_env/bin/activate
+
+# Running the job
+start=`date +%s`
+
+python3 -m momfpriors.run -y configs/exp_configs/all_yahpo_lcbench.yaml -e "all_ids_yahpo_lcbench_25"
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo Job execution complete.
+echo Runtime: $runtime
