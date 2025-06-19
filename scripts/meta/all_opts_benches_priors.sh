289c16808105e73af2e824855ccbb4a8b9653b58
Sohambasu07 <soham.basu07@gmail.com>
2025-06-19 12:01:14 +0200

    Exp configs to run everything

--- scripts/meta/all_opts_benches_priors.sh
+++ scripts/meta/all_opts_benches_priors.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 # Define the partition on which the job shall run.
-#SBATCH --partition mlhiwidlc_gpu-rtx2080    # short: -p mlhiwidlc_gpu-rtx2080
+#SBATCH --partition bosch_cpu-cascadelake    # short: -p bosch_cpu-cascadelake
 
 # Define a name for your job
-#SBATCH --job-name MOMFBO_test            #  short: -J MOMFBO_test
+#SBATCH --job-name PriMO_comp_all            #  short: -J PriMO_comp_all
 
 # Define the files to write the outputs of the job to.
 # Please note the SLURM will not create this directory for you, and if it is missing, no logs will be saved.
 # You must create the directory yourself. In this case, that means you have to create the "logs" directory yourself.
 
-#SBATCH --output logs/%x-%A_meta-momfbo.out   # STDOUT  %x and %A will be replaced by the job name and job id, respectively. short: -o logs/%x-%A_meta-momfbo.out
-#SBATCH --error logs/%x-%A_meta-momfbo.err    # STDERR  short: -e logs/%x-%A_meta-momfbo.out
+#SBATCH --output logs/%x-%A_meta.out   # STDOUT  %x and %A will be replaced by the job name and job id, respectively. short: -o logs/%x-%A_meta.out
+#SBATCH --error logs/%x-%A_meta.err    # STDERR  short: -e logs/%x-%A_meta.out
 
-# Since using GPU Partion, define the number of gpus required per node
-#SBATCH --gpus=4
-#SBATCH --cpus-per-gpu=6
+# Since using CPU Partion, define the number of cpus required per node
+#SBATCH --cpus-per-task 30
 
 echo "Workingdir: $PWD";
 echo "Started at $(date)";
 
 # A few SLURM variables
-echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node, $SLURM_GPUS gpus with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
 
 # Activate your environment
 # You can also comment out this line, and activate your environment in the login node before submitting the job
@@ -30,7 +29,7 @@ source ~/repos/momfp_env/bin/activate
 # Running the job
 start=`date +%s`
 
-python3 -m momfpriors.run -y configs/exp_configs/all_momf.yaml -e "momfbo_test"
+python3 -m momfpriors.run -y configs/exp_configs/all_opts_benches_priors.yaml -e "all_20_evals"
 
 end=`date +%s`
 runtime=$((end-start))
