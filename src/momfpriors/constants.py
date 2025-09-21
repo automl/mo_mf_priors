211472d72249b97de08bf25a803662b4a42a2712
Sohambasu07 <soham.basu07@gmail.com>
2025-09-21 14:52:03 +0200

    Add default scripts dir

--- src/momfpriors/constants.py
+++ src/momfpriors/constants.py
@@ -5,3 +5,4 @@ from pathlib import Path
 DEFAULT_ROOT_DIR = Path.cwd().absolute()
 DEFAULT_PRIORS_DIR = DEFAULT_ROOT_DIR / "src" / "priors"
 DEFAULT_RESULTS_DIR = DEFAULT_ROOT_DIR.parent / "momf_priors_results"
+DEFAULT_SCRIPTS_DIR = DEFAULT_ROOT_DIR / "scripts" / "meta"
